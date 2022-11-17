8b9ecf43964c899088374ff17bf6d990eb41e7fa
jeesoo422 <118365672+jeesoo422@users.noreply.github.com>
2022-11-17 21:28:21 +0900

    cors 부활

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express')
-//var cors = require('cors')
+var cors = require('cors')
 const app = express()
 const port = 3000
 
-//app.use(cors())
+app.use(cors())
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
